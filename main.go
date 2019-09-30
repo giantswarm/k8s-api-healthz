1ae7166e02e81d9ce10d293372429813f6c4b6b2
Vaclav Rozsypalek <vaclav@giantswarm.io>
2019-09-30 10:28:32 +0200

    use pflag instead of flag

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
 
 	"github.com/giantswarm/microerror"
+	flag "github.com/spf13/pflag"
 )
+
+type Flag struct {
+	ApiEndpoint    string
+	ApiCertPath    string
+	ApiCACertPath  string
+	ApiKeyPath     string
+	EtcdEndpoint   string
+	EtcdCertPath   string
+	EtcdCACertPath string
+	EtcdKeyPath    string
+	Port           int
+}
 
 func main() {
 	err := mainError()
@@ -18,17 +30,18 @@ func main() {
 func mainError() error {
 	var err error
 
-	apiEndpoint := flag.String("api-endpoint", "https://127.0.0.1:443/healthz", "K8s API url that will be used for the api connection. Only secure connection is supported.")
-	apiCertPath := flag.String("api-cert", "/etc/kubernetes/ssl/apiserver-crt.pem", "Path to the cert file to authenticate against api.")
-	apiCACertPath := flag.String("api-ca-cert", "/etc/kubernetes/ssl/apiserver-ca.pem", "Path to the cacert file to authenticate against api.")
-	apiKeyPath := flag.String("api-key", "/etc/kubernetes/ssl/apiserver-key.pem", "Path to the key file to authenticate against api.")
-	etcdEndpoint := flag.String("etcd-endpoint", "https://127.0.0.1:2379/health", "ETCD url that will be used for the etcd connection. Only secure connection is supported.")
-	etcdCertPath := flag.String("etcd-cert", "/etc/kubernetes/ssl/apiserver-crt.pem", "Path to the cert file to authenticate against etcd.")
-	etcdCACertPath := flag.String("etcd-ca-cert", "/etc/kubernetes/ssl/apiserver-ca.pem", "Path to the cacert file to authenticate against etcd.")
-	etcdKeyPath := flag.String("etcd-key", "/etc/kubernetes/ssl/apiserver-key.pem", "Path to the key file to authenticate against etcd.")
-	port := flag.Int("port", 8089, "Define a port on which the http server will be running.")
+	var f Flag
+	flag.StringVar(&f.ApiEndpoint, "api-endpoint", "https://127.0.0.1:443/healthz", "K8s API url that will be used for the api connection. Only secure connection is supported.")
+	flag.StringVar(&f.ApiCertPath, "api-cert", "/etc/kubernetes/ssl/apiserver-crt.pem", "Path to the cert file to authenticate against api.")
+	flag.StringVar(&f.ApiCACertPath, "api-ca-cert", "/etc/kubernetes/ssl/apiserver-ca.pem", "Path to the cacert file to authenticate against api.")
+	flag.StringVar(&f.ApiKeyPath, "api-key", "/etc/kubernetes/ssl/apiserver-key.pem", "Path to the key file to authenticate against api.")
+	flag.StringVar(&f.EtcdEndpoint, "etcd-endpoint", "https://127.0.0.1:2379/health", "ETCD url that will be used for the etcd connection. Only secure connection is supported.")
+	flag.StringVar(&f.EtcdCertPath, "etcd-cert", "/etc/kubernetes/ssl/apiserver-crt.pem", "Path to the cert file to authenticate against etcd.")
+	flag.StringVar(&f.EtcdCACertPath, "etcd-ca-cert", "/etc/kubernetes/ssl/apiserver-ca.pem", "Path to the cacert file to authenticate against etcd.")
+	flag.StringVar(&f.EtcdKeyPath, "etcd-key", "/etc/kubernetes/ssl/apiserver-key.pem", "Path to the key file to authenticate against etcd.")
+	flag.IntVar(&f.Port, "port", 8089, "Define a port on which the http server will be running.")
 
-	if os.Args[1] == "version" {
+	if len(os.Args) > 1 && os.Args[1] == "version" {
 		showVersion()
 		return nil
 	}
@@ -41,15 +54,15 @@ func mainError() error {
 	var healthz *Healthz
 	{
 		healthzConfig := HealtzConfig{
-			ApiEndpoint:    *apiEndpoint,
-			ApiCertPath:    *apiCertPath,
-			ApiCACertPath:  *apiCACertPath,
-			ApiKeyPath:     *apiKeyPath,
-			EtcdEndpoint:   *etcdEndpoint,
-			EtcdCertPath:   *etcdCertPath,
-			EtcdCACertPath: *etcdCACertPath,
-			EtcdKeyPath:    *etcdKeyPath,
-			Port:           *port,
+			ApiEndpoint:    f.EtcdEndpoint,
+			ApiCertPath:    f.ApiCertPath,
+			ApiCACertPath:  f.ApiCACertPath,
+			ApiKeyPath:     f.ApiKeyPath,
+			EtcdEndpoint:   f.EtcdEndpoint,
+			EtcdCertPath:   f.EtcdCertPath,
+			EtcdCACertPath: f.EtcdCACertPath,
+			EtcdKeyPath:    f.EtcdKeyPath,
+			Port:           f.Port,
 		}
 
 		healthz, err = NewHealtz(healthzConfig)
