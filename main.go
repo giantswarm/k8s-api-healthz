d3e6ddfef205e78b4ba11a78f397fd513c8fca79
calvix <vaclav@giantswarm.io>
2019-10-01 09:31:54 +0200

    Update main.go
    Co-Authored-By: Marian Steinbach <marian@giantswarm.io>

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func mainError() error {
 	flag.StringVar(&f.ApiCACertPath, "api-ca-cert", "/etc/kubernetes/ssl/apiserver-ca.pem", "Path to the cacert file to authenticate against api.")
 	flag.StringVar(&f.ApiKeyPath, "api-key", "/etc/kubernetes/ssl/apiserver-key.pem", "Path to the private key file to authenticate with against the API.")
 	flag.StringVar(&f.EtcdEndpoint, "etcd-endpoint", "https://127.0.0.1:2379/health", "URL that will be used for the etcd connection. Only a secure connection is supported.")
-	flag.StringVar(&f.EtcdCertPath, "etcd-cert", "/etc/kubernetes/ssl/apiserver-crt.pem", "Path to the cert file to authenticate against etcd.")
+	flag.StringVar(&f.EtcdCertPath, "etcd-cert", "/etc/kubernetes/ssl/apiserver-crt.pem", "Path to the client certificate file to authenticate with against etcd.")
 	flag.StringVar(&f.EtcdCACertPath, "etcd-ca-cert", "/etc/kubernetes/ssl/apiserver-ca.pem", "Path to the CA file containing the issuer of the client certificate passed via --etcd-cert.")
 	flag.StringVar(&f.EtcdKeyPath, "etcd-key", "/etc/kubernetes/ssl/apiserver-key.pem", "Path to the key file to authenticate against etcd.")
 	flag.IntVar(&f.Port, "port", 8089, "Define a port on which the http server will be running.")
