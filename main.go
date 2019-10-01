fceaf74b38c73c1b2dce9cf79c3dc948ab95d977
calvix <vaclav@giantswarm.io>
2019-10-01 09:31:03 +0200

    Update main.go
    Co-Authored-By: Marian Steinbach <marian@giantswarm.io>

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func mainError() error {
 	var err error
 
 	var f Flag
-	flag.StringVar(&f.ApiEndpoint, "api-endpoint", "https://127.0.0.1:443/healthz", "K8s API url that will be used for the api connection. Only secure connection is supported.")
+	flag.StringVar(&f.ApiEndpoint, "api-endpoint", "https://127.0.0.1:443/healthz", "K8s API url that will be used for the API connection. Only a secure connection is supported.")
 	flag.StringVar(&f.ApiCertPath, "api-cert", "/etc/kubernetes/ssl/apiserver-crt.pem", "Path to the cert file to authenticate against api.")
 	flag.StringVar(&f.ApiCACertPath, "api-ca-cert", "/etc/kubernetes/ssl/apiserver-ca.pem", "Path to the cacert file to authenticate against api.")
 	flag.StringVar(&f.ApiKeyPath, "api-key", "/etc/kubernetes/ssl/apiserver-key.pem", "Path to the key file to authenticate against api.")
