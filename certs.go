ef269503e3efa1cc369eea13d926ccde1c82eb72
Volodymyr Stoiko <me@volodymyrstoiko.com>
2020-06-30 16:08:35 +0300

    Use latest orb 0.9.0 (#7)
    * Update changelog
    
    * Remove vendoring
    
    * Apply goimports
    
    * Reaplce microerror maskf
    
    * Fix errors

--- certs.go
+++ certs.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"crypto/x509"
-	"github.com/giantswarm/microerror"
 	"io/ioutil"
+
+	"github.com/giantswarm/microerror"
 )
 
 // CertPoolFromFile returns an x509.CertPool containing the certificates
@@ -13,11 +14,11 @@ import (
 func CertPoolFromFile(filename string) (*x509.CertPool, error) {
 	b, err := ioutil.ReadFile(filename)
 	if err != nil {
-		return nil, microerror.Maskf(err, "can't read CA file: %v", filename)
+		return nil, microerror.Mask(err)
 	}
 	cp := x509.NewCertPool()
 	if !cp.AppendCertsFromPEM(b) {
-		return nil, microerror.Maskf(err, "failed to append certificates from file: %s", filename)
+		return nil, microerror.Mask(err)
 	}
 	return cp, nil
 }
--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/giantswarm/k8s-api-healthz/pkg/project"
 	"os"
+
+	"github.com/giantswarm/k8s-api-healthz/pkg/project"
 
 	"github.com/giantswarm/microerror"
 	flag "github.com/spf13/pflag"
